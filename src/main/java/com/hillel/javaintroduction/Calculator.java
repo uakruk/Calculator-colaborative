aaf27615ae8eda5c25e24906bb321bf5690184c0
pivovar <Sanya550>
2020-10-24 13:02:48 +0300

    calculator pivovar modulesgit st

--- src/main/java/com/hillel/javaintroduction/Calculator.java
+++ src/main/java/com/hillel/javaintroduction/Calculator.java
@@ -5,6 +5,7 @@ public class Calculator {
     public static double module(double a,double b){
         double psresult;
         psresult = a%b;
+        System.out.println(psresult);
         return psresult;
     }
 
