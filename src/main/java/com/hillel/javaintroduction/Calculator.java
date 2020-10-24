e852e083f4970e93adb69ca94f3872e493441dd4
pivovar <sanya550>
2020-10-24 12:54:34 +0300

    calculator pivovar modules

---
+++ src/main/java/com/hillel/javaintroduction/Calculator.java
@@ -0,0 +1,11 @@
+package com.hillel.javaintroduction;
+
+public class Calculator {
+
+    public static double module(double a,double b){
+        double psresult;
+        psresult = a%b;
+        return psresult;
+    }
+
+}
