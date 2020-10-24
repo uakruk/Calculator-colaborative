be78474973739cbc33f86681368f564458f72b8d
Yaroslav Kruk <yaroslav.kruk@ligadigital.com>
2020-10-24 15:46:37 +0300

    removed unattended changes

--- src/main/java/com/hillel/javaintroduction/lesson5/Calculator.java
+++ src/main/java/com/hillel/javaintroduction/lesson5/Calculator.java
@@ -2,21 +2,4 @@ package com.hillel.javaintroduction.lesson5;
 
 public class Calculator {
 
-    public static double modules(double a, double b) {
-        double psresult;
-
-        psresult = a % b;
-        System.out.println(psresult);
-        return psresult;
-
-    }
-    public static double signChange(double signChange){
-      signChange = 0;
-      double resultBer = - signChange;
-      System.out.println(resultBer);
-      return resultBer;
-
-
-    }
-
 }
