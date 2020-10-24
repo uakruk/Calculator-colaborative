5901ddb86963f06a5ea4c95dae9e8fd7d011c36b
pivovar <Sanya550>
2020-10-24 15:20:28 +0300

    22222

--- src/main/java/com/hillel/javaintroduction/Calculator.java
+++ src/main/java/com/hillel/javaintroduction/Calculator.java
@@ -4,9 +4,12 @@ public class Calculator {
 
     public static double module(double a,double b){
         double psresult;
+
         psresult = a%b;
         System.out.println(psresult);
+        System.out.println("результат:");
         return psresult;
+
     }
 
 }
