60a22a6b28b3c0c59adf5ae0c132c016d65bbe70
pivovar <Sanya550>
2020-10-24 15:41:12 +0300

    modules pivovar2

--- src/main/java/com/hillel/javaintroduction/lesson5/Calculator.java
+++ src/main/java/com/hillel/javaintroduction/lesson5/Calculator.java
@@ -2,15 +2,21 @@ package com.hillel.javaintroduction.lesson5;
 
 public class Calculator {
 
-        public static double module(double a,double b){
-            double psresult;
+    public static double modules(double a, double b) {
+        double psresult;
 
-            psresult = a%b;
-            System.out.println(psresult);
-            return psresult;
+        psresult = a % b;
+        System.out.println(psresult);
+        return psresult;
 
-        }
+    }
+    public static double signChange(double signChange){
+      signChange = 0;
+      double resultBer = - signChange;
+      System.out.println(resultBer);
+      return resultBer;
+
 
     }
 
-
+}
