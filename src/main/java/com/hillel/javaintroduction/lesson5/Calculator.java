4a96d3eb3474a56da026103eca001278a8ef2b9c
alexeyananchenko <LQp\(BRVtL5_!yV>
2020-10-24 15:42:28 +0300

    divide update 2 formating changes

--- src/main/java/com/hillel/javaintroduction/lesson5/Calculator.java
+++ src/main/java/com/hillel/javaintroduction/lesson5/Calculator.java
@@ -4,25 +4,20 @@ public class Calculator {
 
     public static void main(String[] args) {
 
-
-
-
-
     }
 
-    public static double divide( double a, double b){
-
+    public static double divide(double a, double b){
         double resultAn = 0;
         resultAn = a / b;
+
         return resultAn;
-
-
     }
-    public static double multiply (double a, double b){
 
+    public static double multiply(double a, double b) {
         double resultAn = 0;
         resultAn = a * b;
-        return resultAn;
 
+        return resultAn;
     }
+
 }
