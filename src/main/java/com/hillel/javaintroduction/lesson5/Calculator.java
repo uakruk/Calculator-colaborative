3ca16f5af0862a2e8b33c67ddf0caad1ce8cba0a
Yaroslav Kruk <yaroslav.kruk@ligadigital.com>
2020-10-24 14:22:25 +0300

    Added Calcluator.java

---
+++ src/main/java/com/hillel/javaintroduction/lesson5/Calculator.java
@@ -0,0 +1,17 @@
+package com.hillel.javaintroduction.lesson5;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        double result = 0;
+        double a;
+        double b;
+
+        resultKrol = a - b;
+
+        return result;
+
+
+
+    }
+}
