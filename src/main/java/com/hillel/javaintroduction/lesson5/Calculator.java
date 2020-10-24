5ad36a04f2caff3a36aab2ec04d0f24e0932b552
kim-alterpost <kim@alterpost.org>
2020-10-24 15:05:49 +0300

    3

--- src/main/java/com/hillel/javaintroduction/lesson5/Calculator.java
+++ src/main/java/com/hillel/javaintroduction/lesson5/Calculator.java
@@ -3,6 +3,23 @@ package com.hillel.javaintroduction.lesson5;
 public class Calculator {
 
     public static void main(String[] args) {
+        stepen(3, 2);
+        stepen(3, 1);
+        stepen(2,3);
+        stepen(3,0);
+        stepen(3, -1);
 
+    }
+
+    public static double stepen(double number, int degree) {
+        if (degree > 0) {
+            for (int i = 1; i < degree; i++) {
+                number = number * number;
+            }
+        } else {
+            number = 0;
+        }
+        System.out.println(number);
+        return number;
     }
 }
