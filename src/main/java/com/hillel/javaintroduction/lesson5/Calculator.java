0452c1ae67cbeb1234641658605add2839812ec8
Lizamin <elyzavetaminakova@gmail.com>
2020-10-24 12:06:09 +0300

    singChange

--- src/main/java/com/hillel/javaintroduction/lesson5/Calculator.java
+++ src/main/java/com/hillel/javaintroduction/lesson5/Calculator.java
@@ -3,9 +3,18 @@ package com.hillel.javaintroduction.lesson5;
 public class Calculator {
 
     public static void main(String[] args) {
-        double signChange = 0;
-        double BERresult = -signChange;
-        System.out.println(BERresult);
 
+
+    }
+    public  static double signChange(double signChange) {
+        signChange = 0;
+        double resultBer = -signChange;
+        double resultBer = resultBer;
+        return resultBer;
+
+    }
+    public static double modulus(double a, double b){
+        double resultMod = a % b;
+        return resultMod;
     }
 }
