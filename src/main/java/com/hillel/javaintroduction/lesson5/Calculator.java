10e3e050b4e66ec24dc8301e9fcbd94bc98526c6
Lizamin <elyzavetaminakova@gmail.com>
2020-10-24 11:46:45 +0300

    singChange

--- src/main/java/com/hillel/javaintroduction/lesson5/Calculator.java
+++ src/main/java/com/hillel/javaintroduction/lesson5/Calculator.java
@@ -3,6 +3,9 @@ package com.hillel.javaintroduction.lesson5;
 public class Calculator {
 
     public static void main(String[] args) {
+        double signChange = 0;
+        double BERresult = -signChange;
+        System.out.println(BERresult);
 
     }
 }
