a30c1ef1acd7ba64f9a650d889e978538181074d
BohdanovychD <dima.bogdanovych2001@gmail.com>
2020-10-24 14:51:41 +0300

    Multiply

--- src/main/java/com/hillel/javaintroduction/lesson5/Calculator.java
+++ src/main/java/com/hillel/javaintroduction/lesson5/Calculator.java
@@ -4,5 +4,13 @@ public class Calculator {
 
     public static void main(String[] args) {
 
+        }
+
+    public static double multiply(double a, double b) {
+        double DBresult = 0;
+        DBresult = a * b;
+        return DBresult;
+
+
     }
 }
