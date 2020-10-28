4a5f618cf382a517d6258ceb227eb8584f3f6c23
Yaroslav Kruk <yaroslav.kruk@ligadigital.com>
2020-10-28 17:12:31 +0200

    Prettified code

--- src/main/java/com/hillel/javaintroduction/lesson5/Calculator.java
+++ src/main/java/com/hillel/javaintroduction/lesson5/Calculator.java
@@ -5,11 +5,18 @@ public class Calculator {
     public static void main(String[] args) {
 }
 
-public static double plus (double a, double b) { double resultViktor =0; resultViktor = a+b; return resultViktor}
+public static double plus(double a, double b) {
+        double resultViktor = 0;
+        resultViktor = a+b;
+        return resultViktor
+    }
 
 
-public static double minus (double a,double b) {double resultViktor =0; resultViktor = a-b; return resultViktor}
-
+public static double minus(double a, double b) {
+        double resultViktor =0;
+        resultViktor = a-b;
+        return resultViktor
+    }
 }
 
 
