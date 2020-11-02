7fa4e9042529f1e194d748558c6cd3bf95b44790
Pyvovar Sasha <pivovarsasa9@gmail.com>
2020-11-02 14:00:35 +0200

    pyvovar

--- src/main/java/com/hillel/javaintroduction/lesson5/Calculator.java
+++ src/main/java/com/hillel/javaintroduction/lesson5/Calculator.java
@@ -1,83 +1,23 @@
 package com.hillel.javaintroduction.lesson5;
 
-import java.util.Scanner;
-
 public class Calculator {
 
-    enum Operation {
-        PLUS,
-        MINUS,
-        MULTIPLY,
-        DIVIDE,
-        MODULUS,
-        STEPEN,
-        SIGN_CHANGE;
+    public static double modules(double a, double b) {
+
+        double psresult;
+
+        psresult = a % b;
+        System.out.println(psresult);
+        return psresult;
+    }
+    public static double signChange(double signChange) {
+        signChange = 0;
+        double resultBer = -signChange;
+        System.out.println(resultBer);
+        return resultBer;
+
+
     }
 
-    public static void main(String[] args) {
-
-        Operation operation = null;
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter the operation you want to perform:");
-
-        String read = scanner.nextLine();
-        char operationChar = read.charAt(0);
-
-        switch (operationChar) {
-            case '+':
-                operation = Operation.PLUS;
-                break;
-            case '-':
-                operation = Operation.MINUS;
-                break;
-            case '/':
-                operation = Operation.DIVIDE;
-                break;
-            case '*':
-                operation = Operation.MULTIPLY;
-                break;
-            case '%':
-                operation = Operation.MODULUS;
-                break;
-            case '^':
-                operation = Operation.STEPEN;
-                break;
-            case '~':
-                operation = Operation.SIGN_CHANGE;
-                break;
-            default:
-                System.out.println("Unkonwn operation, aborting.");
-                System.exit(0);
-        }
-
-        double firstNumber, secondNumber;
-
-        boolean requiresSecondNumber = true;
-        if (operation == Operation.SIGN_CHANGE) {
-            requiresSecondNumber = false;
-        }
-
-        System.out.println("Enter the first number: ");
-        firstNumber = Double.parseDouble(scanner.nextLine());
-
-        double result = 0;
-
-        if (requiresSecondNumber) {
-            System.out.println("Enter the second number: ");
-            secondNumber = Double.parseDouble(scanner.nextLine());
-
-            switch (operation) {
-                // your operations
-                case PLUS:
-                    // and so on and so on
-                default:
-                    System.out.println("Aborting, unknown operation.");
-            }
-        } else {
-            // call the signChange() method here
-        }
-
-        System.out.println("Result = " + result);
-    }
 }
+
