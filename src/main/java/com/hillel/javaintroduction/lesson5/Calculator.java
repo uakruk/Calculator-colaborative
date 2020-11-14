96a86a8bfa88062060632098bfb4209a6264c475
Aleks0309 <aberdin0309@gmail.com>
2020-11-14 11:55:09 +0200

    'calculator'

--- src/main/java/com/hillel/javaintroduction/lesson5/Calculator.java
+++ src/main/java/com/hillel/javaintroduction/lesson5/Calculator.java
@@ -51,7 +51,7 @@ public class Calculator {
                 System.exit(0);
         }
 
-        double firstNumber, secondNumber;
+        double firstNumber = 0, secondNumber = 0;
 
         boolean requiresSecondNumber = true;
         if (operation == Operation.SIGN_CHANGE) {
@@ -70,29 +70,60 @@ public class Calculator {
             switch (operation) {
                 // your operations
                 case PLUS:
-                    // and so on and so on
+                    System.out.println("Result = " + plus(firstNumber, secondNumber));
+                    break;
+                case MINUS:
+                    System.out.println("Result = " + minus(firstNumber, secondNumber));
+                    break;
+                case DIVIDE:
+                    System.out.println("Result = " + divide(firstNumber, secondNumber));
+                    break;
+                case MULTIPLY:
+                    System.out.println("Result = " + multiply(firstNumber, secondNumber));
+                    break;
+                case MODULUS:
+                    System.out.println("Result = " + modulus(firstNumber, secondNumber));
+                    break;
+
+                // and so on and so on
                 default:
                     System.out.println("Aborting, unknown operation.");
             }
         } else {
-            // call the signChange() method here
+            System.out.println("Result = " + signChange(firstNumber));
         }
-
-        System.out.println("Result = " + result);
     }
-    public  static double signChange(double signChange) {
-        signChange = 0;
-        double resultBer = -signChange;
-        System.out.println(resultBer);
-
+    public static double signChange(double a) {
+        double resultBer;
+        resultBer = -a;
         return resultBer;
 
     }
     public static double modulus(double a, double b){
-        double resultMod = a % b;
-        System.out.println(resultMod);
-
+        double resultMod;
+        resultMod = a % b;
         return resultMod;
     }
+    public static double plus (double a, double b){
+        double resultPlus;
+        resultPlus = a + b;
+        return resultPlus;
+    }
+    public static double minus (double a, double b){
+        double resultMinus;
+        resultMinus = a - b;
+        return resultMinus;
+    }
+    public static double divide (double a, double b){
+        double resultDivide;
+        resultDivide = a / b;
+        return resultDivide;
+    }
+    public static double multiply (double a, double b){
+        double resultMultiply;
+        resultMultiply = a * b;
+        return resultMultiply;
+    }
+
 
 }
